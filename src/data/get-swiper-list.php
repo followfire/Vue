d8790b02d6d9c2e3caaee6c1d3ffd64c7155dbbb
XZM <xuzhimeng@qq.com>
2018-01-04 22:11:10 +0800

    注释掉测试语句

--- src/data/get-swiper-list.php
+++ src/data/get-swiper-list.php
@@ -1,7 +1,7 @@
 <?php
   require('init.php');
   $sql = "select m_statistics.m_id,m_list.m_image from m_statistics join m_list on m_list.m_id=m_statistics.m_id order by m_statistics.m_stat_count desc limit 5";
-  var_dump($sql);
+//   var_dump($sql);
   $result = mysqli_query($conn,$sql);
   while($row=mysqli_fetch_assoc($result)){
         $output[]=$row;
